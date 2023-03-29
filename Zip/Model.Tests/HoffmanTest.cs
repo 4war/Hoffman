9d99990fc847017f74be33536ecc91ddfa472f2f
4war <fedro_777@mail.ru>
2023-03-29 09:25:10 +0400

    Created some projects

---
+++ Zip/Model.Tests/HoffmanTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Model.Tests
+{
+    [TestFixture]
+    public class HoffmanTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
